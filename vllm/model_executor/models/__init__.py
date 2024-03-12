02f0b796d325280ddb8a8b7f04a16b5afe999a46
meghagarwal <16129366+megha95@users.noreply.github.com>
2024-03-12 17:06:22 +0000

    databricks name change

--- vllm/model_executor/models/__init__.py
+++ vllm/model_executor/models/__init__.py
@@ -34,6 +34,7 @@ _MODELS = {
     "MixtralForCausalLM": ("mixtral", "MixtralForCausalLM"),
     "QuantMixtralForCausalLM": ("mixtral_quant", "MixtralForCausalLM"),
     "MPT2ForCausalLM": ("mpt2", "MPT2ForCausalLM"),
+    "DatabricksForCausalLM": ("databricks", "DatabricksForCausalLM"),
     # transformers's mpt class has lower case
     "MptForCausalLM": ("mpt", "MPTForCausalLM"),
     "MPTForCausalLM": ("mpt", "MPTForCausalLM"),
