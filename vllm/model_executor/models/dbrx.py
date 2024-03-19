f6ee20b1af495d0f8ca12c17017fc6d152e62cc8
Cheng Li <pistasable@gmail.com>
2024-03-19 06:15:16 +0000

    wip

--- vllm/model_executor/models/dbrx.py
+++ vllm/model_executor/models/dbrx.py
@@ -4,13 +4,15 @@ from typing import List, Optional, Tuple
 
 import torch
 import torch.nn as nn
+from typing import Callable
 
 from vllm.model_executor.input_metadata import InputMetadata
 from vllm.model_executor.layers.attention import Attention
 from vllm.model_executor.layers.linear import (LinearMethodBase,
                                                QKVParallelLinear,
                                                RowParallelLinear,
-                                               ReplicatedLinear)
+                                               ReplicatedLinear,
+                                               UnquantizedLinearMethod, MergedColumnParallelLinear)
 from vllm.model_executor.layers.fused_moe import fused_moe
 from vllm.model_executor.layers.sampler import Sampler
 from vllm.model_executor.layers.vocab_parallel_embedding import (
@@ -54,6 +56,77 @@ class DbrxRouter(nn.Module):
         router_logits, _ = self.layer(hidden_states)
         return router_logits
 
+class DbrxExpertMLP(torch.nn.Module):
+
+    def __init__(self, hidden_size: int, ffn_hidden_size: int,
+                 activation_fn: Callable[[torch.Tensor], torch.Tensor]):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.ffn_hidden_size = ffn_hidden_size
+        self.activation_fn = activation_fn
+
+        self.up_proj = ReplicatedLinear(
+            in_features=hidden_size,
+            out_features=ffn_hidden_size,
+            bias=False,
+        )
+        self.down_proj = ReplicatedLinear(
+            in_features=ffn_hidden_size,
+            out_features=hidden_size,
+            bias=False,
+        )
+        self.gate_proj = ReplicatedLinear(
+            in_features=hidden_size,
+            out_features=ffn_hidden_size,
+            bias=False,
+        )
+
+    def forward(self, x):
+        x1 = self.up_proj(x)
+        x2 = self.gate_proj(x)
+        x1 = self.activation_fn(x1)
+        x1 = x1 * x2
+        x1 = self.down_proj(x1)
+        return x1
+
+class DbrxMoE(nn.Module):
+
+    def __init__(self, hidden_size: int, ffn_hidden_size: int,
+                 moe_num_experts: int, activation_fn: Callable[[torch.Tensor],
+                                                               torch.Tensor]):
+        super().__init__()
+        self.moe_num_experts = moe_num_experts
+        self.activation_fn = activation_fn
+        self.mlp = nn.ModuleList([
+            DbrxExpertMLP(hidden_size=hidden_size,
+                                ffn_hidden_size=ffn_hidden_size,
+                                activation_fn=self.activation_fn)
+            for i in range(self.moe_num_experts)
+        ])
+
+    def forward(self, x, weights, top_weights, top_experts):
+        bsz, q_len, hidden_size = x.shape
+        x = x.view(-1, hidden_size)
+        out = torch.zeros_like(x)
+
+        expert_mask = nn.functional.one_hot(
+            top_experts, num_classes=self.moe_num_experts).permute(2, 1, 0)
+        for expert_idx in range(0, self.moe_num_experts):
+            topk_idx, token_idx = torch.where(expert_mask[expert_idx])
+            if token_idx.shape[0] == 0:
+                continue
+
+            token_list = token_idx.tolist()
+            topk_list = topk_idx.tolist()
+
+            expert_tokens = x[None, token_list].reshape(-1, hidden_size)
+            expert_out = self.mlp[expert_idx](expert_tokens) * top_weights[
+                token_list, topk_list, None]
+
+            out.index_add_(0, token_idx, expert_out)
+
+        out = out.reshape(bsz, q_len, hidden_size)
+        return out
 
 class DbrxExperts(nn.Module):
     """A tensor-parallel MoE implementation for DBRX that shards each expert
@@ -81,26 +154,44 @@ class DbrxExperts(nn.Module):
             params_dtype = torch.get_default_dtype()
         self.params_dtype = params_dtype
 
-        self.router = DbrxRouter(config, self.params_dtype)
-        self.ws = nn.Parameter(
-            torch.empty(self.num_total_experts,
-                        2 * self.intermediate_size,
-                        self.d_model,
-                        device="cuda",
-                        dtype=self.params_dtype))
-        self.w2s = nn.Parameter(
-            torch.empty(self.num_total_experts,
-                        self.d_model,
-                        self.intermediate_size,
-                        device="cuda",
-                        dtype=self.params_dtype))
+        self.linear_method = linear_method
+        if self.linear_method is None:
+            self.linear_method = UnquantizedLinearMethod()
 
-        set_weight_attrs(self.ws, {
-            "weight_loader": self.weight_loader,
-        })
-        set_weight_attrs(self.w2s, {
-            "weight_loader": self.weight_loader,
-        })
+        self.router = DbrxRouter(config, self.params_dtype)
+
+        if not self.linear_method.quant_config.support_fused_moe():
+            self.ws = nn.Parameter(
+                torch.empty(self.num_total_experts,
+                            2 * self.intermediate_size,
+                            self.d_model,
+                            device="cuda",
+                            dtype=self.params_dtype))
+            self.w2s = nn.Parameter(
+                torch.empty(self.num_total_experts,
+                            self.d_model,
+                            self.intermediate_size,
+                            device="cuda",
+                            dtype=self.params_dtype))
+
+            set_weight_attrs(self.ws, {
+                "weight_loader": self.weight_loader,
+            })
+            set_weight_attrs(self.w2s, {
+                "weight_loader": self.weight_loader,
+            })
+        else:
+            self.ws = MergedColumnParallelLinear(self.d_model,
+                                                    [self.intermediate_size] * 2,
+                                                    bias=False,
+                                                    linear_method=linear_method,
+                                                    num_experts=self.num_total_experts)
+            self.w2s = RowParallelLinear(self.intermediate_size,
+                                            self.d_model,
+                                            bias=False,
+                                            linear_method=linear_method,
+                                            num_experts=self.num_total_experts)
+
 
     def weight_loader(self, param: nn.Parameter, loaded_weight: torch.Tensor,
                       weight_name: str):
@@ -118,32 +209,44 @@ class DbrxExperts(nn.Module):
                 loaded_weight,
                 [-1, self.intermediate_size * self.tp_size, self.d_model])
             param_data[:,
-                       shard_size:2 * shard_size, :] = loaded_weight[:,
-                                                                     shard, :]
+                    shard_size:2 * shard_size, :] = loaded_weight[:,
+                                                                    shard, :]
         if weight_name.endswith("w2"):
             loaded_weight = torch.reshape(
                 loaded_weight,
                 [-1, self.intermediate_size * self.tp_size, self.d_model
-                 ]).transpose(1, 2)
+                ]).transpose(1, 2)
             param_data[:] = loaded_weight[:, :, shard]
+
 
     def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
         batch_size, sequence_length, hidden_size = hidden_states.shape
         hidden_states = hidden_states.view(-1, self.d_model)
         # router_logits: (batch * sequence_length, n_experts)
         router_logits = self.router(hidden_states)
-        final_hidden_states = fused_moe(hidden_states,
-                                        self.ws,
-                                        self.w2s,
-                                        router_logits,
-                                        self.top_k,
-                                        renormalize=True,
-                                        inplace=True)
 
-        if self.tp_size > 1:
-            final_hidden_states = tensor_model_parallel_all_reduce(
-                final_hidden_states)
+        if not self.linear_method.quant_config.support_fused_moe():
+            final_hidden_states = fused_moe(hidden_states,
+                                            self.ws,
+                                            self.w2s,
+                                            router_logits,
+                                            self.top_k,
+                                            renormalize=True,
+                                            inplace=True)
 
+            if self.tp_size > 1:
+                final_hidden_states = tensor_model_parallel_all_reduce(
+                    final_hidden_states)
+
+        else:
+            final_hidden_states = self.linear_method.apply_moe_weights(
+                self.ws.linear_weights,
+                self.w2s.linear_weights,
+                self.d_model,
+                router_logits,
+                self.top_k,
+                renormalize=True,
+            )
         return final_hidden_states.view(batch_size, sequence_length,
                                         hidden_size)
 
@@ -376,24 +479,64 @@ class DbrxForCausalLM(nn.Module):
                      cache_dir: Optional[str] = None,
                      load_format: str = "auto",
                      revision: Optional[str] = None):
-        expert_params_mapping = [
-            ("ws" if weight_name in ["w1", "v1"] else "w2s",
-             f"experts.mlp.{weight_name}")
-            for weight_name in ["w1", "v1", "w2"]
-        ]
+
         params_dict = dict(self.named_parameters(remove_duplicate=False))
-        for name, loaded_weight in hf_model_weights_iterator(
-                model_name_or_path, cache_dir, load_format, revision):
-            for param_name, weight_name in expert_params_mapping:
-                if weight_name not in name:
-                    continue
-                name = name.replace(weight_name, param_name)
-                param = params_dict[name]
-                weight_loader = param.weight_loader
-                weight_loader(param, loaded_weight, weight_name)
-                break
-            else:
-                param = params_dict[name]
-                weight_loader = getattr(param, "weight_loader",
-                                        default_weight_loader)
-                weight_loader(param, loaded_weight)
+
+        if self.linear_method is None or (
+            self.linear_method.quant_config.support_fused_moe()):
+            expert_params_mapping = [
+            # (param_name, weight_name, shard_id, expert_id)
+            ("ws" if weight_name in ["up_proj", "gate_proj"] else "w2s",
+             f"experts.mlp.{expert_id}.{weight_name}", shard_id, expert_id)
+            for expert_id in range(self.config.num_total_experts)
+            for weight_name, shard_id in [("up_proj", 0), ("gate_proj", 1), ("down_proj", None)]
+            ]
+
+            for name, loaded_weight in hf_model_weights_iterator(
+                    model_name_or_path, cache_dir, load_format, revision):
+                for (param_name, weight_name, shard_id,
+                     expert_id) in expert_params_mapping:
+                    if weight_name not in name:
+                        continue
+                    name = name.replace(weight_name, param_name)
+                    if name.endswith(".bias") and name not in params_dict:
+                        continue
+                    param = params_dict[name]
+                    weight_loader = param.weight_loader
+                    if shard_id is None:
+                        weight_loader(param,
+                                      loaded_weight,
+                                      expert_id=expert_id)
+                    else:
+                        weight_loader(param,
+                                      loaded_weight,
+                                      shard_id,
+                                      expert_id=expert_id)
+                    break
+                else:
+                    param = params_dict[name]
+                    weight_loader = getattr(param, "weight_loader",
+                                            default_weight_loader)
+                    weight_loader(param, loaded_weight)
+        else:
+            expert_params_mapping = [
+                ("ws" if weight_name in ["w1", "v1"] else "w2s",
+                f"experts.mlp.{weight_name}")
+                for weight_name in ["w1", "v1", "w2"]
+            ]
+
+            for name, loaded_weight in hf_model_weights_iterator(
+                    model_name_or_path, cache_dir, load_format, revision):
+                for param_name, weight_name in expert_params_mapping:
+                    if weight_name not in name:
+                        continue
+                    name = name.replace(weight_name, param_name)
+                    param = params_dict[name]
+                    weight_loader = param.weight_loader
+                    weight_loader(param, loaded_weight, weight_name)
+                    break
+                else:
+                    param = params_dict[name]
+                    weight_loader = getattr(param, "weight_loader",
+                                            default_weight_loader)
+                    weight_loader(param, loaded_weight)
